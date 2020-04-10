04bf50978ba15472baac83100dff476980468ecc
ram <ramkama30@gmail.com>
2020-04-10 17:15:44 +0100

    Update Providers

--- providers.tf
+++ providers.tf
@@ -1,4 +1,13 @@
 provider "aws" {
   version = "~> 2.0"
-  //region  = "us-east-1"
+  region = var.region
+}
+
+//S3 backend configuration
+terraform {
+  backend "s3" {
+    bucket = "aws-tf-state"
+    key    = "aws-tf-prj01"
+    region = "eu-west-1"
+  }
 }
